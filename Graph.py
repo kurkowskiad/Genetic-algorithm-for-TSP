a92390d453da700f817a4091148714121023204f
adriankurkowski <adrian.kurkowski@dreamstormstudios.com>
2019-12-25 21:13:05 +0100

    Basic visualisation

--- Graph.py
+++ Graph.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import random
 import math
 
+names = ["Istanbul", "Moscow", "London", "Petersburg", "Berlin", "Madrid", "Kyiv", "Rome", "Paris", "Bucharest",
+         "Minsk", "Hamburg", "Vienna", "Warsaw", "Budapest", "Barcelona", "Kharkiv", "Munich", "Milan", "Prague"]
 
 class Node:
     def __init__(self, x, y, name=""):
@@ -19,25 +21,27 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, count, show_edges=True, show_edge_labels=True):
+    def __init__(self, count, show_edges=False, show_edge_labels=False):
         self.nodes = [Node(x=random.random(), y=random.random(), name=names[i]) for i in range(count)]
-        self.edges = []
+        self.edges = self.create_edges()
         self.show_edges = show_edges
         self.show_edge_labels = show_edge_labels
-        self.create_edges()
         self.graph = nx.Graph()
 
     def create_edges(self):
+        edges = []
         for index1 in range(len(self.nodes)):
             index2 = 0
             while index2 < len(self.nodes):
                 if index1 == index2 or index2 < index1:
                     pass
                 else:
-                    self.edges.append(Edge(self.nodes[index1], self.nodes[index2],
+                    edges.append(Edge(self.nodes[index1], self.nodes[index2],
                                            weight=math.sqrt((self.nodes[index2].x-self.nodes[index1].x)**2
                                                             +(self.nodes[index2].y-self.nodes[index1].y)**2)))
                 index2 += 1
+        return edges
+
     def initialize_all(self):
         # Add nodes.
         for node in self.nodes:
@@ -48,22 +52,19 @@ class Graph:
             # Add edges.
             for edge in self.edges:
                 self.graph.add_edge(edge.node1.name, edge.node2.name, weight=edge.weight)
-
             if self.show_edge_labels:
                 edge_labels = nx.get_edge_attributes(self.graph, 'weight')
+                # Draw edge labels.
                 nx.draw_networkx_edge_labels(self.graph, pos=pos, edge_labels=edge_labels)
-            nx.draw(self.graph, pos=pos, with_labels=True)
-        else:
-            nx.draw(self.graph, pos=pos, with_labels=True)
+
+        # Draw nodes.
+        nx.draw(self.graph, pos=pos, with_labels=True)
 
         plt.xlim(left=-0.02, right=1.02)
         plt.ylim(bottom=-0.02, top=1.02)
         plt.show()
 
-
 if __name__ == "__main__":
     # Node names must be unique, otherwise the node won't be added to the graph.
-    names = ["Istanbul", "Moscow", "London", "Petersburg", "Berlin", "Madrid", "Kyiv", "Rome", "Paris", "Bucharest",
-             "Minsk", "Hamburg", "Vienna", "Warsaw", "Budapest", "Barcelona", "Kharkiv", "Munich", "Milan", "Prague"]
-    g = Graph(4, show_edges=True)
+    g = Graph(5)
     g.initialize_all()
